afa612a8bf0e2f96b9893a3455df990695586004
江州安抚使 <wanghuizhou21@163.com>
2021-09-09 17:02:07 +0800

    修改bug
    修改bug

---
+++ UIView+UIViewUtils.m
@@ -0,0 +1,180 @@
+//
+//  UIView+UIViewUtils.m
+//  HZProgress
+//
+//  Created by 王会洲 on 16/4/8.
+//  Copyright © 2016年 王会洲. All rights reserved.
+//
+
+#import "UIView+UIViewUtils.h"
+
+typedef enum : NSUInteger {
+    MSHUDStyleDefault, //头部有圆圈显示
+    MSHUDStyleLabel, //只显示文字内容
+    MSHUDStyleNormal ,    //常用样式
+} MSShowHUDViewStyle;
+
+
+#define kMSHUDTOPWINDOW [self getCurrentWindowView]
+
+@implementation UIView (UIViewUtils)
+
+/// 显示文字提示（自动隐藏）
+/// @param indiTitle 提醒文字
+- (void)showHUDIndicatorAtCenter:(NSString *)indiTitle {
+    MSProgressHUD *hud = [self getHUDIndicatorViewAtCenter];
+    if (hud) {
+        [hud hide:YES]; // 如果有已经展示的隐藏
+    }
+    hud = [self createHUDIndicatorViewAtCenter:indiTitle HUDCase:MSHUDStyleLabel];
+    [hud show:YES];
+    [self hide:YES afterDelay:1.25];
+}
+
+/// 网络发起提示（不自动隐藏）
+/// @param indiTitle 提醒文字
+- (void)showHUDIndicatorSendAtCenter:(NSString *)indiTitle {
+    MSProgressHUD *hud = [self getHUDIndicatorViewAtCenter];
+    if (hud) {
+        // 如果有已经展示的隐藏
+        [hud hide:YES];
+    }
+    hud = [self createHUDIndicatorViewAtCenter:indiTitle HUDCase:MSHUDStyleDefault];
+    [hud show:YES];
+}
+
+/// 只显示文字提示（自动隐藏）
+/// @param indiTitle 提示文案
+/// @param y 偏移量
+- (void)showHUDIndicatorViewAtCenter:(NSString *)indiTitle yOffset:(CGFloat)y {
+    MSProgressHUD *hud = [self getHUDIndicatorViewAtCenter];
+    if (hud) {
+        // 如果有已经展示的隐藏
+        [hud hide:YES];
+    } else {
+        hud.labelText = indiTitle;
+    }
+    hud = [self createHUDIndicatorViewAtCenter:indiTitle icon:nil yOffset:y MPBCase:MSHUDStyleLabel];
+    [hud show:YES];
+    [self hide:YES afterDelay:1.25];
+}
+
+
+
+/**隐藏弹层*/
+- (void)hideHUDIndicatorViewAtCenter {
+    MSProgressHUD *hud = [self getHUDIndicatorViewAtCenter];
+    [hud hide:YES];
+}
+
+/**延迟加载控制*/
+- (void)hide:(BOOL)animated afterDelay:(NSTimeInterval)delay {
+    [self performSelector:@selector(hideDelayed:) withObject:[NSNumber numberWithBool:animated] afterDelay:delay];
+}
+
+- (void)hideDelayed:(NSNumber *)animated {
+    [self hideHUDIndicatorViewAtCenter];
+}
+
+
+
+/// 创建HUD对象
+/// @param indiTitle indiTitle 标题
+/// @param ShowHUDStyle 显示样式
+- (MSProgressHUD *)createHUDIndicatorViewAtCenter:(NSString *)indiTitle
+                                          HUDCase:(MSShowHUDViewStyle)ShowHUDStyle {
+    return [self createHUDIndicatorViewAtCenter:indiTitle icon:nil yOffset:0 MPBCase:ShowHUDStyle];
+}
+
+/// 创建HUD对象
+/// @param indiTitle 标题
+/// @param icon 图标
+/// @param y y轴偏移量
+/// @param ShowHUDStyle 显示样式
+- (MSProgressHUD *)createHUDIndicatorViewAtCenter:(NSString *)indiTitle
+                                             icon:(NSString *)icon
+                                          yOffset:(CGFloat)y
+                                          MPBCase:(MSShowHUDViewStyle)ShowHUDStyle {
+    MSProgressHUD *hud = [[MSProgressHUD alloc] initWithView:self];
+    hud.layer.zPosition = 10;
+    hud.yOffset = y;
+    hud.removeFromSuperViewOnHide = YES;
+    hud.labelText = indiTitle;
+    
+    /**如果有图片名称*/
+    if (icon) {
+        hud.customView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:[NSString stringWithFormat:@"MSProgress.bundle/%@", icon]]];
+    }
+    switch (ShowHUDStyle) {
+        case MSHUDStyleDefault:
+        {
+            hud.mode = MSProgressHUD25X;
+            hud.dimBackground = NO;
+            hud.color = nil;
+            hud.labelColor = [UIColor whiteColor];
+        }
+            break;
+        case MSHUDStyleLabel:
+        {
+            hud.mode = MSProgressHUDModeText;
+            hud.dimBackground = NO;
+            hud.color = nil;
+            hud.labelColor = [UIColor whiteColor];
+        }
+            break;
+            
+        case MSHUDStyleNormal:
+        {
+            hud.mode = MSProgressHUDModeNormal;
+            hud.dimBackground = NO;
+            hud.color = [UIColor whiteColor];
+            hud.labelColor = [UIColor blackColor];
+        }
+            break;
+        default:
+            break;
+    }
+    [self addSubview:hud];
+    hud.tag = hudViewTag;
+    return hud;
+}
+
+
+/// 获取HUD对象
+- (MSProgressHUD *)getHUDIndicatorViewAtCenter {
+    UIView *view = [self viewWithTagNotDeepCounting:hudViewTag];
+    if (view != nil && [view isKindOfClass:[MSProgressHUD class]]){
+        return (MSProgressHUD *)view;
+    } else {
+        return nil;
+    }
+}
+
+- (UIView *)viewWithTagNotDeepCounting:(NSInteger)tag {
+    for (UIView *view in self.subviews) {
+        if (view.tag == tag) {
+            return view;
+            break;
+        }
+    }
+    return nil;
+}
+
+
+
+/**获取当前window*/
+- (UIWindow *)getCurrentWindowView {
+    UIWindow * window = [[UIApplication sharedApplication] keyWindow];
+    if (window.windowLevel != UIWindowLevelNormal) {
+        NSArray *windows = [[UIApplication sharedApplication] windows];
+        for(UIWindow * tmpWin in windows) {
+            if (tmpWin.windowLevel == UIWindowLevelNormal) {
+                window = tmpWin;
+                break;
+            }
+        }
+    }
+    return window;
+}
+
+@end
